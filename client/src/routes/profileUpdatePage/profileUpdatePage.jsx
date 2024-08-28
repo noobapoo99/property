9226ef3a1dcce0aed7415e9b204f4374a9f381f2
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-28 13:38:35 +0530

    profileUpdatepage

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,25 +1,31 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 import "./profileUpdatePage.scss";
 
 function ProfileUpdatePage() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="profileUpdatePage">
       <div className="formContainer">
         <form>
           <h1>Update Profile</h1>
           <div className="item">
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">username</label>
             <input
               id="username"
               name="username"
               type="text"
+              defaultValue={currentUser.username}
             />
           </div>
           <div className="item">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">email</label>
             <input
               id="email"
               name="email"
               type="email"
+              defaultValue={currentUser.email}
             />
           </div>
           <div className="item">
@@ -30,7 +36,11 @@ function ProfileUpdatePage() {
         </form>
       </div>
       <div className="sideContainer">
-        <img src="" alt="" className="avatar" />
+        <img
+          src={currentUser.avatar || "/logo.png"}
+          alt=""
+          className="avatar"
+        />
       </div>
     </div>
   );
