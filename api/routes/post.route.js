ca32d4d881dd6e589b3e50dbfa948ec1dccb06d7
noobapoo99 <apoorvtripathi99@gmail.com>
2024-08-29 22:40:04 +0530

    post_controller

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,5 +1,12 @@
 import express from "express";
 import { verifyToken } from "../middleware/verifyToken.js";
+import {
+  addPost,
+  deletePost,
+  getPost,
+  getPosts,
+  updatePost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
